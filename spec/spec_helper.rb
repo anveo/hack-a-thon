3195b0054f344002e54e05dd423358cbee89d293
Brian Racer <bracer@gmail.com>
2017-04-18 11:28:20 -0600

    gems and scaffolding

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rack/test'
+require_relative '../lib/app'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.include Rack::Test::Methods
+end
